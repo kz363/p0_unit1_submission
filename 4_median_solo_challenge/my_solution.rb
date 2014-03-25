a38d2e234730ee2dd180502d9f615ff13c50ae35
kz363 <kz363@nyu.edu>
2014-03-25 14:51:29 -0400

    challenge 4 finished

--- 4_median_solo_challenge/my_solution.rb
+++ 4_median_solo_challenge/my_solution.rb
@@ -12,14 +12,32 @@
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
+# Create a function, median, that takes an array as an input and returns the median of the array.
+# Sort the array.
+# If the array is of odd length, return the value at array.length/2
+# Else, return the sum of the two middle values.
 
 # 2. Initial Solution
 
-
+# def median(array)
+#   array.sort!
+#   if array.length % 2 != 0
+#     return array[array.length/2]
+#   else
+#     return (array[array.length/2] + array[array.length/2 - 1]) / 2.0
+#   end
+# end
 
 
 # 3. Refactored Solution
 
-
+def median(array)
+  array.sort!
+  return array[array.length/2] if array.length % 2 != 0
+  return (array[array.length/2] + array[array.length/2 - 1]) / 2.0
+end
 
 # 4. Reflection 
+# This challenge was relatively simple since we've already had to find the median of an array. 
+# Everything went smoothly as a result. I'm not sure if the refactored code is necessarily 
+# refactored, however. I just saw the opportunity to use less lines and I went for it.
