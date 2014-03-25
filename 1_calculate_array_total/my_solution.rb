ea84f668a74a9faeca169bcd8f723adf8e44823a
kz363 <kz363@nyu.edu>
2014-03-25 12:54:15 -0400

    first 2 challenges finished

--- 1_calculate_array_total/my_solution.rb
+++ 1_calculate_array_total/my_solution.rb
@@ -4,7 +4,7 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
@@ -12,14 +12,54 @@
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
+# Create a function, total, that takes an array as an input and returns 
+# the sum of all the elements in the array.
+#   - Create a variable, sum equal to 0
+#   - For each element in the array, add to sum
+#   - Return sum
+
+# Create a function, sentence_maker, that takes an array of strings or numbers 
+# and returns a string of each element joined into a sentence.
+#   - Call the join method on the array with a space as the separator and store 
+#     it into a variable called string
+#   - Capitalize the first letter 
+#   - Concatenate a period.
+#   - Return string
 
 # 2. Initial Solution
 
+# def total(array)
+#   sum = 0
+#   array.each do |i|
+#     sum += i
+#   end
+#   sum
+# end
 
-
+# def sentence_maker(array)
+#   string = array.join(" ")
+#   string[0] = string[0].capitalize
+#   string += '.'
+# end
 
 # 3. Refactored Solution
 
+def total(array)
+  array.reduce(:+)
+end
 
+def sentence_maker(array)
+  string = array.join(" ") + '.'
+  string[0] = string[0].upcase
+  string
+end
 
 # 4. Reflection 
+
+# My initial code worked out well because I followed my pseudocode. After I wrote 
+# my code, I kept wondering if Ruby had any built in methods to make any of this 
+# easier. the array.reduce methods was one that I found to sum arrays easily. I 
+# still don't know if my sentence_maker is optimal, but it's the best I can do for 
+# now. I'm confident in the learning objectives. I enjoyed finding new methods 
+# to do stuff in Ruby. I'm in the mindset that Ruby has a method to do everything, 
+# because it's been true for my small amount of Ruby experience so far.
--- 2_calculate_letter_grade/my_solution.rb
+++ 2_calculate_letter_grade/my_solution.rb
@@ -4,7 +4,7 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
@@ -12,14 +12,40 @@
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
+# Create a function, get_grade, that takes an array as an input. Store 
+# the average of the array into a variable called array.
+#   -If average is >= 90, return "A"
+#   -If average is >= 80, return "B"
+#   -If average is >= 70, return "C"
+#   -If average is >= 60, return "D"
+#   -If average is < 60, return "F"
 
 # 2. Initial Solution
 
-
-
+# def get_grade(array)
+#   average = array.reduce(:+) / array.length
+#   return "A" if average >= 90
+#   return "B" if average >= 80
+#   return "C" if average >= 70
+#   return "D" if average >= 60
+#   return "F" if average < 60
+# end
 
 # 3. Refactored Solution
 
-
+def get_grade(array)
+  average = array.reduce(:+) / array.length
+  return "A" if average >= 90
+  return "B" if average >= 80
+  return "C" if average >= 70
+  return "D" if average >= 60
+  return "F"
+end
 
 # 4. Reflection 
+
+# I was deciding between using if/elsif and cases at first, but I decided 
+# to keep it simple and just use if statements since the code will terminate 
+# when it returns something anyway. My refactored version has a very minimal 
+# change, but the code remains functionally the same. I was able to use the 
+# new method I learned from the last challenge so that was cool.
