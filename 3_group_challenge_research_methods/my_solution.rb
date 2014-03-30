162fb0a9ff97e8c95d29af52a906b38cd650e520
kz363 <kz363@nyu.edu>
2014-03-29 20:26:09 -0400

    edited group challenge

--- 3_group_challenge_research_methods/my_solution.rb
+++ 3_group_challenge_research_methods/my_solution.rb
@@ -23,29 +23,45 @@ end
 # can use do-end as well. You can use |x| in the block to indicate how you want 
 # to reference each element in your code, or in the case of hashes, |x, y|.
 
+# Person 1's solution (Ken)
+# def my_array_finding_method(source, thing_to_find)
+#    source.select { |x| x.to_s.include? thing_to_find}
+# end
 
-# Person 2
-def my_array_modification_method(source, thing_to_modify)
-  # Your code here!
-end
-
-def my_hash_modification_method(source, thing_to_modify)
-  # Your code here!
-end
+# def my_hash_finding_method(source, thing_to_find)
+#    hash = source.select { |k,v| v == thing_to_find }
+#    output = hash.keys
+# end
 
 # Identify and describe the ruby method you implemented. 
-# 
-#
-#
+# The Ruby method I implemented was select. It selects all the elements of an array/hash
+# that returns true in the block. Using that, I was able to find and return the elements
+# from the data structures that I want.
+
+
+# Person 2
+def my_array_modification_method(source, number)
+  source.map!{|element| element.is_a?(Integer) ? element + number : element}
+end
+
+def my_hash_modification_method(source, number)
+  source.update(source){|key,value| value +number}
+end
+
+# I used two ruby methods, map! for the array and update for the hash.
+# (map! returns an array, which is why it couldn't be used for the hash method.)
+# map! runs the code in the block for each element of the enumerable object, changing the underlying array.
+# In this case, the block code is a ternary operator which modifies only if the element is an Integer.
+# The update method performs a similar operation on a hash object.
 
 
 # Person 3
-def my_array_sorting_method(source, thing_to_locate)
-  # Your code here!
+def my_array_sorting_method(source)
+  source.sort { |a,b| a <=> b } 
 end
 
-def my_hash_sorting_method(source, thing_to_locate)
-  # Your code here!
+def my_hash_sorting_method(source, thing_to_sort)
+  source.sort_by { |name, age| age } 
 end
 
 # Identify and describe the ruby method you implemented. 
@@ -77,11 +93,11 @@ p my_array_finding_method(i_want_pets, "t") == ["want","pets","but"]
 p my_hash_finding_method(my_family_pets_ages, 3) == ["Hoobie", "Ditto"]
 p my_array_modification_method(i_want_pets, 1) == ["I", "want", 4, "pets", "but", "I", "only", "have", 3 ]
 p my_hash_modification_method(my_family_pets_ages, 2) == {"Evi" => 8, "Hoobie" => 5, "George" => 14, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5}
-p my_array_sorting_method(i_want_pets, "pets") == ["3", "4", "I", "but", "have", "only", "pets", "want"]
-p my_hash_sorting_method(my_family_pets_ages, age) == [["Annabelle", 2], ["Ditto", 5], ["Hoobie", 5], ["Bogart", 6], ["Poly", 6], ["Evi", 8], ["George", 14]] 
+p my_array_sorting_method(i_want_pets) == ["3", "4", "I", "but", "have", "only", "pets", "want"]
+p my_hash_sorting_method(my_family_pets_ages) == [["Annabelle", 2], ["Ditto", 5], ["Hoobie", 5], ["Bogart", 6], ["Poly", 6], ["Evi", 8], ["George", 14]]
 # This may be false depending on how your method deals with ordering the animals with the same ages.
-p my_deletion_method(i_want_pets, "a") == ["I", 4, "pets", "but", "I", "only", 3 ]
-p my_deletion_method(my_family_pets_ages, "George") == {"Evi" => 8, "Hoobie" => 5, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5}
+p my_array_deletion_method(i_want_pets, "a") == ["I", 4, "pets", "but", "I", "only", 3 ]
+p my_hash_deletion_method(my_family_pets_ages, "George") == {"Evi" => 8, "Hoobie" => 5, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5}
 
 # Reflect!
 # I'm not sure if I used the right method for my Person 1 part, but I couldn't think of 
@@ -91,3 +107,8 @@ p my_deletion_method(my_family_pets_ages, "George") == {"Evi" => 8, "Hoobie" => 
 # one line, but you can see the result up there. This was the first time I used unless 
 # in my code because it felt more like natural English than if I were to use if, so that 
 # was cool.
+
+# UPDATE
+# Another person in my group, Ken, was also Person 1 and used the select method to complete 
+# the code. I'm glad he was able the find the proper method to do it because I felt like 
+# using each was a cop out.
